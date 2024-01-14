076d9fae32184e6373ba67dee74147cfc183b6e8
Alexander Fischer <alexander-fischer1801@t-online.de>
2024-01-14 12:16:07 +0100

    fix error in tests

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -6,13 +6,13 @@ test_that("test cluster input", {
   library(fixest)
   library(wildrwolf)
   
-  dqrng::dqset.seed(123)
+  set.seed(123)
   models1 = feols(c(vs, am) ~ mpg | cyl, mtcars, cluster = "carb")
-  rwolf1 = rwolf(models = models, param = "mpg", B = 9999)
+  rwolf1 = rwolf(models = models1, param = "mpg", B = 999)
   
-  dqrng::dqset.seed(123)
+  set.seed(123)
   models2 = feols(c(vs, am) ~ mpg | cyl, mtcars, cluster = ~carb)
-  rwolf2 = rwolf(models = models, param = "mpg", B = 9999)
+  rwolf2 = rwolf(models = models2, param = "mpg", B = 999)
   
   expect_equal(rwolf1, rwolf2)
   
